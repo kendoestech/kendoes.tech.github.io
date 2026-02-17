9eea188f2a52cab789dd96344cafa2813c135837
kendoestech <48156024+kendoestech@users.noreply.github.com>
2026-02-17 11:01:18 -0700

    Adding falldetect folder for testing

---
+++ falldetect/settings.js
@@ -0,0 +1,8 @@
+var DEFAULT_SETTINGS = {
+    "host": "2509120a0a8c445fb79e7bfa868c9891.s1.eu.hivemq.cloud",
+    "port": "8884",
+    "username": "FallDetectBridgeService",
+    "password": "m3JdF@fDSy@bE84",
+    "topic": "falldetect/test",
+    "deviceId": "7T4FTG"
+};
